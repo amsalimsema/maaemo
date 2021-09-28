f0a8f9c2d47cfb2f094b7f0c6109b226558070ef
Salim Semakula <salimsema@yahoo.com>
2021-09-28 22:20:34 +0300

    testFlight

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Home from './Pages/Home'
+import Home from './pages/Home'
 import NavTop from './components/Nav'
 import ScrollToTop from './components/ScrollToTop'
 import './App.css'
